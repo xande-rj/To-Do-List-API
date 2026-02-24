601e4da95b664a79f0cf451c65fe07c00528652d
alexandre <alexandresargitario@gmail.com>
2026-02-24 11:15:52 -0300

    feat: dto usuario adicionado

---
+++ src/main/java/alexandreS/To_Do_List_API/DTO/usuarioDTO.java
@@ -0,0 +1,13 @@
+package alexandreS.To_Do_List_API.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class usuarioDTO {
+    private  String emailUsuario;
+    private String nomeUsuario;
+}
