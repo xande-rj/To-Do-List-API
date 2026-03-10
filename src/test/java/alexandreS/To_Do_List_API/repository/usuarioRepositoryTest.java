c0954e4c63aaf342c0a34c675d7b645d2c4a2eab
alexandre <alexandresargitario@gmail.com>
2026-03-10 19:02:21 -0300

    test: teste de repositorio do usuario

---
+++ src/test/java/alexandreS/To_Do_List_API/repository/usuarioRepositoryTest.java
@@ -0,0 +1,78 @@
+package alexandreS.To_Do_List_API.repository;
+
+import alexandreS.To_Do_List_API.DTOS.usuarioCadastroDTO;
+import alexandreS.To_Do_List_API.entitys.usuarioEntity;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class usuarioRepositoryTest {
+    @Autowired
+    usuarioRepository repository;
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Test
+    @DisplayName("Usario ja existe no banco de dados retorna TRUE")
+    void existsByEmailUsuarioSuccesses() {
+        usuarioCadastroDTO data = new usuarioCadastroDTO("teste@gmail.com","nomeTeste","testeSenha123");
+        this.createUser(data);
+boolean result = this.repository.existsByEmailUsuario(data.getEmailUsuario());
+
+assertThat(result).isTrue();
+
+    }
+
+    @Test
+    @DisplayName("Usario nao existe no banco de dados retorna FALSE")
+    void existsByEmailUsuarioFail() {
+        usuarioCadastroDTO data = new usuarioCadastroDTO("teste@gmail.com","nomeTeste","testeSenha123");
+
+        boolean result = this.repository.existsByEmailUsuario(data.getEmailUsuario());
+
+        assertThat(result).isFalse();
+
+    }
+
+    @Test
+    @DisplayName("Usuario no banco existe")
+    void findByEmailUsuarioSucesses() {
+        usuarioCadastroDTO data = new usuarioCadastroDTO("teste@gmail.com","nomeTeste","testeSenha123");
+        this.createUser(data);
+        Optional<usuarioEntity> result = this.repository.findByEmailUsuario(data.getEmailUsuario());
+
+        assertThat(result.isPresent()).isTrue();
+
+    }
+
+    @Test
+    @DisplayName("Usuario nao banco nao existe")
+    void findByEmailUsuarioFail() {
+        usuarioCadastroDTO data = new usuarioCadastroDTO("teste@gmail.com","nomeTeste","testeSenha123");
+
+        Optional<usuarioEntity> result = this.repository.findByEmailUsuario(data.getEmailUsuario());
+
+        assertThat(result.isEmpty()).isTrue();
+
+    }
+
+    private usuarioEntity createUser(usuarioCadastroDTO data){
+        usuarioEntity user = new usuarioEntity();
+        user.setEmailUsuario(data.getEmailUsuario());
+        user.setNomeUsuario(data.getNomeUsuario());
+        user.setSenhaUsuario(data.getSenhaUsuario());
+        this.entityManager.persist(user);
+        return user;
+    }
+
+}
