2981df10a3209be8bcb02240bf35a716f9176c3f
alexandre <alexandresargitario@gmail.com>
2026-03-11 11:45:42 -0300

    tests: testes de controller usuario

---
+++ src/test/java/alexandreS/To_Do_List_API/controller/usuarioControllerTest.java
@@ -0,0 +1,12 @@
+package alexandreS.To_Do_List_API.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class usuarioControllerTest {
+
+    @Test
+    void saveUsuario() {
+    }
+}
